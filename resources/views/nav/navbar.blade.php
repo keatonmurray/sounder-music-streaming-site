0ebf54207644297688e966aa31cd25e7419b4ce4
Kate <murraykeaton422@gmail.com>
2023-11-16 04:18:33 -0500

    used bootstrap to make navbar responsive

--- resources/views/nav/navbar.blade.php
+++ resources/views/nav/navbar.blade.php
@@ -1,12 +1,26 @@
-<div class="container">
-    <nav class="navbar">
-        <a class="navbar-brand" href="/" style="color:#fff;">Sounder</a>
-        <ul>
-            <li class="active"><a href="/">Sounder Community</a></li>
-            <li><a href="#discover">Artists</a></li>
-            <li><a href="#about-us">How it works</a></li>
-            <li><a href="#features">App Features</a></li>
-            <li class="btn"><a href="#">Signup for free</a></li>
+<nav class="navbar navbar-expand-lg d-lg-flex">
+    <div class="container">
+      <a class="navbar-brand" href="#">Sounder</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+        <span><i class="fa-solid fa-bars" style="color: #ffffff;"></i></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample07">
+        <ul class="navbar-nav mt-1">
+          <li class="nav-item">
+            <a class="nav-link" id="active" href="#">Sounder community</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Artists</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">How it works</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">App features</a>
+          </li>
         </ul>
-    </nav>
-</div>
+        <a class="btn" href="">Signup for free</a>
+      </div>
+    </div>
+  </nav>
