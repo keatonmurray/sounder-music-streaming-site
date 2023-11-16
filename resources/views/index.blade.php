8734bdd33898bc34d4b33634f5dc65d4fc1168b8
Kate <murraykeaton422@gmail.com>
2023-11-16 06:10:25 -0500

    front cover image is now responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,26 @@
 @extends('main.layout')
 @section('content')
+@php
+  use Illuminate\Support\Facades\Vite;
+@endphp
 <body>
-    <header>
-        <section>
-            <div class="container col-xxl-8 px-4 py-5">
-                <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-                <div class="col-10 col-sm-8 col-lg-6"></div>
-                <div class="col-lg-6">
-                    <h1 class="display-5 fw-bold gs-2 lh-1 mb-3">A platform for independent musicians</h1>
-                    <p class="lead fs-6">Stream your favourite music, support independent artists, or sell your own music. Sounder is an artist-first music streaming platform that lets you keep more of your earnings.</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <button type="button" class="btn btn-light btn-lg px-4 me-md-2">Browse music</button>
-                        <button type="button" class="btn btn-outline-light btn-lg px-4">Learn more</button>
-                    </div>
-                </div>
+    <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6"></div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold gs-2 lh-1 mb-3">A platform for independent musicians</h1>
+                <p class="lead fs-6">Stream your favourite music, support independent artists, or sell your own music. Sounder is an artist-first music streaming platform that lets you keep more of your earnings.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-light btn-lg px-4 me-md-2">Browse music</button>
+                    <button type="button" class="btn btn-outline-light btn-lg px-4">Learn more</button>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+    <div class="image">
+      <img src="{{Vite::asset('resources/images/bg.png')}}" alt="">
+    </div>
+    </section>
     </header>
     @include('about-us')
     @include('app-features')
--- resources/views/nav/navbar.blade.php
+++ resources/views/nav/navbar.blade.php
@@ -1,26 +1,27 @@
-<nav class="navbar navbar-expand-lg d-lg-flex">
-    <div class="container">
-      <a class="navbar-brand" href="#">Sounder</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-        <span><i class="fa-solid fa-bars" style="color: #ffffff;"></i></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExample07">
-        <ul class="navbar-nav mt-1">
-          <li class="nav-item">
-            <a class="nav-link" id="active" href="#">Sounder community</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Artists</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">How it works</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">App features</a>
-          </li>
-        </ul>
-        <a class="btn" href="">Signup for free</a>
+<header>
+  <section id="main">
+    <nav class="navbar navbar-expand-lg d-lg-flex">
+      <div class="container">
+        <a class="navbar-brand" href="#">Sounder</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span><i class="fa-solid fa-bars" style="color: #ffffff;"></i></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+          <ul class="navbar-nav mt-1">
+            <li class="nav-item">
+              <a class="nav-link" id="active" href="#">Sounder community</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Artists</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">How it works</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">App features</a>
+            </li>
+          </ul>
+          <a class="btn" href="">Signup for free</a>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
